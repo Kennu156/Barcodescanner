f9b08cbe92483b3acfb54835cdd89bbfa822cd4c
Kenmartti <145542503+Kennu156@users.noreply.github.com>
2024-01-25 13:50:06 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+// check compatibility
+if (!("BarcodeDetector" in globalThis)) {
+    console.log("Barcode Detector is not supported by this browser.");
+  } else {
+    console.log("Barcode Detector supported!");
+  
+    BarcodeDetector.getSupportedFormats().then((supportedFormats) => {
+        supportedFormats.forEach((format) => console.log(format));
+      });
+
+    // create new detector
+    const barcodeDetector = new BarcodeDetector({
+      formats: ["code_39", "codabar", "ean_13"],
+    });
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+          facingMode: { ideal: 'environment' }
+        },
+        audio: false
+      });
+        const videoEl = document.querySelector('#stream');
+    videoEl.srcObject = stream;
+    await videoEl.play();
+
+      window.setInterval(async () => {
+        const barcodes = await barcodeDetector.detect(videoEl);
+        if (barcodes.length <= 0) return;
+        alert(barcodes.map(barcode => barcode.rawValue));
+      }, 1000)
+
+  }
+ 
